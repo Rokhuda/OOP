750a38fd3907e4378bbfc0be2eefe2b631b4c42e
rokhuda <rokhuda@gmail.com>
2019-07-23 00:13:12 +0200

    added comments

--- spec/diceSpec.js
+++ spec/diceSpec.js
@@ -1,11 +1,15 @@
- describe("dice", () => {
-    it("should make a any type of die", () => {
-        let diceFactory = new DiceFactory(6);
-        diceFactory.makeDie(6);
-        expect(diceFactory.sides.length).toEqual(6);
-    });
-    it("should return true if dice is fair", () => {
-        let diceTester = new DiceTester();
-        expect(diceTester.testDie()).toBeTruthy();
-    });
-}); 
+ describe("die making", () => {
+     it("should make any type of die", () => {
+         let diceFactory = new DiceFactory(6);
+         diceFactory.makeDie(6);
+         expect(diceFactory.sides).toEqual(6);
+     });
+ });
+
+describe("die testing", () => {
+     it("should return true if dice is fair", () => {
+         let diceTester = new DiceTester(6);
+         expect(diceTester.testDie(diceFactory)).toBeTruthy(true);
+     });
+
+ });
--- src/diceSrc.js
+++ src/diceSrc.js
@@ -1,51 +1,46 @@
-// make any type of die
+// This function can create a die with any number of sides
 
 class DiceFactory {
     constructor(sides) {
         this.sides = sides;
     }
     makeDie() {
-        return (Math.floor(Math.random * this.sides) + 1);
+        return (Math.floor(Math.random() * this.sides) + 1);
     }
 }
 
-let diceFactory = new DiceFactory(6);
-console.log(diceFactory.makeDie());
-
 //test die
 
-class DiceTester{
+class DiceTester {
     constructor(dieType) {
-        this.dieType = dieType;
-        this.tally = [];
-    }
-
+        this.dieType = dieType;    //it could be any type of die
+        this.tally = [];           //stores the number of time a die lands on a specific side
+        this.mean = ((this.dieType * 1000) * 0.05); // mean accepted  for a die to be fair
+        this.min = 1000 - this.mean;
+        this.max = 1000 + this.mean;
+    };
     testDie(testingDie) {
-        let diceFactory = new DiceFactory();
-    
-        for (let i = 0; i < this.dieType; I++) {
+        for (let i = 0; i < this.dieType; i++) {
             this.tally[i] = 0;
-
+        };
+        if (this.dieType !== testingDie.sides) {
+            throw Error('the wrong type of die have been input');
+        };
+        for (let i = 0; i < this.dieType * 1000; i++) {     //if a die has 6 sides, the die will be rolled 6000 time to test the randomness of the each side
+            let roll = testingDie.makeDie();               // every roll is tallied
+            this.tally[roll - 1]++;
         }
-
-    if (this.dieType !== testingDie.sides) {
-        throw Error('')
-    }
-    for (let i = 0; i < this.dieType * 1000; i++){
-        let roll = testingDie.makeDie();
-        this.tally[roll - 1]++;
-
-    }
-        for (let i = 0; i < this.tally.length; i++){
-            let mean = ((this.dieType * 1000) * 0.05);
-            let min = 1000 - this.mean;
-            let max = 1000 + max;
+        for (let i = 0; i < this.tally.length; i++) {       //the function checks the minimum and maximum time a side is rolled
             if (this.tally[i] < this.min || this.tally[i] > this.max) {
-                throw Error('die is not fair')
-                }
+                throw Error('die is not fair');
+            }
         }
         return true;
-        }
-} 
-let diceTester = new DiceTester();
-diceTester.testDie();
+    }
+};
+
+
+let diceFactory = new DiceFactory(6);
+console.log(diceFactory.makeDie());
+let diceTester = new DiceTester(6);
+console.log(diceTester.testDie(diceFactory));
